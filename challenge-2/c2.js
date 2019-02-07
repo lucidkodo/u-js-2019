99b640f084c45dad8de07a66159e706a7f174bfc
angie <lucidkodo@gmail.com>
2019-02-07 23:32:27 +0800

    WIP

--- challenge-2/c2.js
+++ challenge-2/c2.js
@@ -4,13 +4,26 @@ class team {
     this.scores = scores
     this.avg = this.scores.reduce((a, b) => a + b, 0) / this.scores.length
   }
-
-  // avgScore () {
-  //   return this.score.reduce((a, b) => a + b, 0) / this.score.length
-  // }
 }
 
 const john = new team('john', [89, 120, 103])
 const mike = new team('mike', [116, 94, 123])
 const mary = new team('mary', [97, 134, 105])
 
+const teams = [ john, mike, mary ]
+// console.log(teams) // [mary, mike, john]
+
+teams.sort((teamA, teamB) => {
+  return teamB.avg - teamA.avg // descending
+})
+
+// since you've sorted, now check if there's any draws
+const scores = teams.map(team => {
+  return {
+    name: team[name],
+    avgScore: team[avg]
+  }
+})
+
+
+console.log(scores)
